8439766b4d72d4034061f273f69ed7e12388d9dc
RoiArthurB <contact@arthurbrugiere.fr>
2018-12-24 18:34:47 +0700

    [ADD] full V1

---
+++ V1/launch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash  
+
+# Run Docker-compose :
+sudo docker-compose up -d
+
+# Launch work in Master container :
+sudo docker exec -ti v1_spark_1 sh -c "/usr/spark-2.3.1/bin/spark-submit --class WordCount --master local /tmp/data/wc.jar /tmp/data/sample1.txt"
+
+# Result :
+echo "Do you want to display result? [y-N]"
+read displayResult
+if [[ $displayResult == "y" ||  $displayResult == "" ]]; then
+	sudo cat ../data/result/*
+fi
+
+# Stop containers
+echo "Do you want to stop containers? [y-N]"
+read stopDocker
+if [[ $stopDocker == "y" || $stopDocker == "" ]]; then
+	sudo docker-compose stop
+fi
